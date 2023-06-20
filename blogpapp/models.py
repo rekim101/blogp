450815c11d2e1d5a994a0648047f178b5bba8b96
rekim101 <trickshj@gmail.com>
2023-06-20 22:14:02 +0300

    adding the publication time function

--- blogpapp/models.py
+++ blogpapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
+class post(models.Model):
+    title=models.CharField(max_length=250)
+    slug=models.SlugField(max_length=250)
+    body=models.TextField()
+    publish=models.DateTimeField(default=timezone.now)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+    class meta :
+        ordering=['-publish']
+
+    def __str__(self):
+        return self.title
 # Create your models here.
