3311e8e1d802dc1fd068f0f0a935899c4e9fb199
rekim101 <trickshj@gmail.com>
2024-02-13 23:58:20 +0300

    added SEO and pagination

--- blogpapp/models.py
+++ blogpapp/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
         PUBLISHED='PB','Published'
 
     title=models.CharField(max_length=250)
-    slug=models.SlugField(max_length=250)
+    slug=models.SlugField(max_length=250, unique_for_date='publish')
     author= models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
     body=models.TextField()
     publish=models.DateTimeField(default=timezone.now)
@@ -29,6 +29,11 @@ class Post(models.Model):
         indexes = [
             models.Index(fields=['-publish']),
         ]
-    def __str__(self):
-        return self.title
+        def __str__(self):
+            return self.title
+        def get_absolute_url(self):
+            return reverse('blog:post_detail', args = [self.id])
+        
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
 # Create your models here.
--- blogpapp/urls.py
+++ blogpapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name= 'blogpapp'
 urlpatterns=[
     path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
-]
+    
+    path('<int:years>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post_detail')
+
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
